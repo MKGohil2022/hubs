a25938a4259b543c9f8c9c38ae67c83128852b94
Mayurdhvajsinh Gohil <mayurdhvajsinh.gohil@einfochips.com>
2022-04-22 14:48:16 +0530

    US: 24 -- reset logo and footer link changes

--- src/react-components/layout/Footer.js
+++ src/react-components/layout/Footer.js
@@ -25,11 +25,11 @@ export function Footer({
           {!hidePoweredBy && (
             <FormattedMessage
               id="footer.powered-by"
-              defaultMessage="Powered by <a>Hubs Cloud</a>"
+              defaultMessage="Powered by <a>SwivelMeta</a>"
               values={{
                 // eslint-disable-next-line react/display-name
                 a: chunks => (
-                  <a className={styles.link} href="https://hubs.mozilla.com/cloud">
+                  <a className={styles.link} href="https://swivelmeta.io" target="_blank" rel="noopener noreferrer">
                     {chunks}
                   </a>
                 )
